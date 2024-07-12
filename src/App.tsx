ac8c6ad33107331cb92806ca19045031b8658c74
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2024-07-12 15:33:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export function App() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      <div className="text-3xl">Testing</div>
+      <div>Please Work!</div>
+    </div>
+  );
+}
